d9b264745e00d1a91030d0ce783cfe969deef21e
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2026-04-29 11:43:42 +0200

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,9 @@
+<?php
+// TODO: Démarrer la session PHP
+
+// TODO: Supprimer le panier de la session
+// Utiliser unset() sur \$_SESSION['panier']
+
+// TODO: Rediriger l'utilisateur vers panier.php
+
+// TODO: Terminer l'exécution du script pour éviter tout affichage après la redirection
